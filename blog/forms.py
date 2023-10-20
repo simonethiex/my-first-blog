660a8c86f834b1cb766175cd6092b176054d4465
Simone <thiex@fh-aachen.de>
2023-10-20 15:37:13 +0200

    adds post_edit function

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+         model = Post
+         fields = ('title', 'text',)
+
+"""class NameForm (forms.Form):
+    your_name = forms.CharField(label="Your name", max_length=100)"""
