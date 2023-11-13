73cf5d96ffb82a858ff7ab76bd7a0bcd54f11ef3
Simone <thiex@fh-aachen.de>
2023-11-13 11:05:59 +0100

    boehchen test

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/boehnchen/', views.post_boehnchen, name='post_boehnchen'),
+    path('post/draft_list', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/delete', views.post_delete, name='post_delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_draft_list(request):
+  #draft_posts= Post.objects.filter(published_date__gt=timezone.now())
+   draft_posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+   return render(request, 'blog/post_draft_list.html', {'posts':draft_posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -22,7 +26,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,9 +40,21 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_boehnchen(request):
+    return render(request, 'blog/boehnchen.html')
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
